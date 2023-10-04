c70b4243468db852c1e6455efb65dbe06a9d7d81
Acasiax <yunji9039@naver.com>
2023-10-05 00:29:22 +0900

    안내사항 작성

---
+++ HBDLaunch/MYInformation.swift
@@ -0,0 +1,18 @@
+//
+//  MYInformation.swift
+//  HBDLaunch
+//
+//  Created by 이윤지 on 10/5/23.
+//
+
+import SwiftUI
+
+struct MYInformation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MYInformation()
+}
