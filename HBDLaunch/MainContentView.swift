0c8e8dd893b9636757eea1b707a4dad7b8768da1
Acasiax <yunji9039@naver.com>
2023-10-04 23:54:39 +0900

    persistant와 cotent뷰를 수정했습니다.

---
+++ HBDLaunch/MainContentView.swift
@@ -0,0 +1,24 @@
+//
+//  MainContentView.swift
+//  ElegantTaskApp
+//
+//  Created by 이윤지 on 2023/04/14.
+//
+
+import SwiftUI
+
+struct MainContentView: View {
+    
+    var body: some View {
+        Home()
+        
+        // MyView의 전체 백그라운드 컬러를 파란색으로 지정
+   
+    }
+}
+
+struct MainContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainContentView()
+    }
+}
